c0beeb784fc916c1e80137c457c6ad1b0d717810
Alex Vovk <alex.vovk@cyberoo.com>
2024-07-24 10:22:19 +0300

    Add cli.

--- setup.py
+++ setup.py
@@ -22,4 +22,11 @@ setup(
         "Operating System :: MacOS :: MacOS X",
     ],
     zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'fib-number = flitton_fib_rs.'
+            'fib_number_command:'
+            'fib_number_command',
+        ],
+    },
 )
