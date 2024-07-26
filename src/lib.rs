0cd8be69b3b838cdcc2c3f178a24704e6e1cdbf5
Alex Vovk <alex.vovk@cyberoo.com>
2024-07-26 09:11:24 +0300

    Add object interface.

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ use fib_calcs::fib_number::__pyo3_get_function_fibonacci_number;
 use fib_calcs::fib_numbers::__pyo3_get_function_fibonacci_numbers;
 mod interface;
 use interface::config::__pyo3_get_function_run_config;
+use interface::object::__pyo3_get_function_object_interface;
 // pub mod fib_numbers;
 
 #[pyfunction]
@@ -19,5 +20,6 @@ fn flitton_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(fibonacci_number));
     m.add_wrapped(wrap_pyfunction!(fibonacci_numbers));
     m.add_wrapped(wrap_pyfunction!(run_config));
+    m.add_wrapped(wrap_pyfunction!(object_interface));
     Ok(())
 }
