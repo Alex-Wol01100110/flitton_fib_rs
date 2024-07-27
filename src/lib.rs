a616ec5ebe19b49d94edcc9088233f74352bd7e9
Alex Vovk <alex.vovk@cyberoo.com>
2024-07-27 09:48:31 +0300

    Numpy use example.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
+use pyo3::types::PyDict;
 
 mod fib_calcs;
 use fib_calcs::fib_number::__pyo3_get_function_fibonacci_number;
@@ -29,6 +30,27 @@ fn time_add_vectors(total_vector_size: i32) -> Vec<i32> {
         buffer.push(first_vector[**&i as usize] +second_vector[*i as usize]);
     }
     buffer
+}
+
+#[pyfunction]
+fn test_numpy<'a>(result_dict: &'a PyDict) -> PyResult<&'a PyDict> {
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+    let locals = PyDict::new(py);
+    locals.set_item("np", py.import("numpy").unwrap());
+
+    let code = "np.array([[3, 2], [1, 4]])";
+    let weights_matrix = py.eval(code, None, Some(&locals)).unwrap();
+    locals.set_item("weights_matrix", weights_matrix);
+
+    let new_code = "np.array([[10], [20]])";
+    let input_matrix = py.eval(new_code, None, Some(&locals)).unwrap();
+    locals.set_item("input_matrix", input_matrix);
+
+    let calc_code = "np.dot(weights_matrix, input_matrix)";
+    let result_end = py.eval(calc_code, None, Some(&locals)).unwrap();
+    result_dict.set_item("numpy result", result_end);
+    Ok(result_dict)
 }
 
 #[pymodule]
