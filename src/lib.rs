93cfa118e3bc41ff347eb01b500be71fa2abdebe
Alex Vovk <alex.vovk@cyberoo.com>
2024-07-27 21:34:54 +0300

    Add numpy model.

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,10 @@ use interface::config::__pyo3_get_function_run_config;
 use interface::object::__pyo3_get_function_object_interface;
 // pub mod fib_numbers;
 mod class_module;
-use class_module::fib_processor::FibProcessor;
+use class_module::fib_processor::FibProcessor;A
+mod numpy_model;
+use numpy_model::__pyo3_get_function_calculate_times;
+use numpy_model::__pyo3_get_function_calculate_parameters;
 
 #[pyfunction]
 fn say_hello() {
@@ -62,6 +65,8 @@ fn flitton_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(object_interface));
     m.add_wrapped(wrap_pyfunction!(time_add_vectors));
     m.add_wrapped(wrap_pyfunction!(test_numpy));
+    m.add_wrapped(wrap_pyfunction!(calculate_times));
+    m.add_wrapped(wrap_pyfunction!(calculate_parameters));
     m.add_class::<FibProcessor>()?;
     Ok(())
 }
